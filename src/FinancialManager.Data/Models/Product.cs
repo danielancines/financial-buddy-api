17a6722f22a2cb4b4dc79f8c56f4425cf4c618dc
Daniel Ancines <daniel.ancines@gmail.com>
2023-02-25 01:00:57 -0300

    Products endpoint

--- src/FinancialManager.Data/Models/Product.cs
+++ src/FinancialManager.Data/Models/Product.cs
@@ -8,7 +8,23 @@ public class Product
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
-    
+
+    [BsonElement("barcode")]
+    public string? Barcode { get; set; }
+
     [BsonElement("name")]
     public string? Name { get; set; }
+
+    [BsonElement("description")]
+    public string? Description { get; set; }
+
+    [BsonElement("price")]
+    public double Price { get; set; }
+
+    [BsonElement("store")]
+    public string? Store { get; set; }
+
+    [BsonElement("date")]
+    public DateTime Date { get; set; }
+
 }
